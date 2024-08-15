287b65ed28e3b92e670ad61493801f2a784fde33
yukihira-ali <ali_asnawi93@yahoo.com>
2024-08-15 23:55:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+function startGame() {
+  const guess = prompt('What does this 🔴🐂 stand for?');
+
+  if (guess.toLowerCase() === 'red bull') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was red bull`);
+  }
+
+  const guess2 = prompt('What does this 🎂👣🐎 stand for?');
+
+  if (guess2.toLowerCase() === 'kek tapak kuda') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was kek tapak kuda`);
+  }
+
+  const guess3 = prompt('What does this 🐔👨‍🍳🟥 stand for?');
+
+  if (guess3.toLowerCase() === 'ayam masak merah') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was ayam masak merah`);
+  }
+
+  const guess4 = prompt('What does this 🍏🥧 stand for?');
+
+  if (guess4.toLowerCase() === 'apple pie') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was apple pie`);
+  }
+
+  const guess5 = prompt('What does this ☠️🐔 stand for?');
+
+  if (guess5.toLowerCase() === 'rangka ayam?') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was rangka ayam?`);
+  }
+
+  const guess6 = prompt('What does this 🌊🗿➕ stand for?');
+
+  if (guess6.toLowerCase() === 'air batu campur') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was air batu campur`);
+  }
+
+  const guess7 = prompt('What does this 👴🥔 stand for?');
+
+  if (guess7.toLowerCase() === 'mister potato') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was mister potato`);
+  }
+
+  const guess8 = prompt('What does this 🐓🧱🥛 stand for?');
+
+  if (guess8.toLowerCase() === 'ayam buttermilk') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was ayam buttermilk`);
+  }
+
+  const guess9 = prompt('What does this 🧠🧠 stand for?');
+
+  if (guess9.toLowerCase() === 'otak-otak') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was otak otak`);
+  }
+
+  const guess10 = prompt('What does this 🍚🐓🥧🐒 stand for?');
+
+  if (guess10.toLowerCase() === 'nasi ayam penyet') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was nasi ayam penyet`);
+  }
+}
